bd6da86c0166c751be121e58c403755a60b71d84
stardustfilms <86339133+stardustfilms@users.noreply.github.com>
2021-06-24 11:34:42 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -100,7 +100,7 @@ async def start(bot, update):
     
     await bot.send_photo(
         chat_id=update.chat.id,
-        photo="https://telegra.ph/file/d5b6d57ae0028cb318f11.jpg"
+        photo="https://telegra.ph/file/d5b6d57ae0028cb318f11.jpg",
         caption=Translation.START_TEXT.format(
                 update.from_user.first_name),
         reply_markup=reply_markup,
